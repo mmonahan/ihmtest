b872adebffdb9690384e70157246117944fd053d
mmonahan <mmonahan@email.com>
2018-09-15 17:16:45 -0500

    Initial test and Swagger UI

--- src/main/java/com/iheart/advertiser/AdvertiserApplication.java
+++ src/main/java/com/iheart/advertiser/AdvertiserApplication.java
@@ -2,9 +2,40 @@ package com.iheart.advertiser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AdvertiserApplication {
+
+    @Bean
+    public Docket apiDocket() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Advertiser Api Service",
+                "a wonderful advertiser service",
+                "0.1.0",
+                "termsOfServiceUrl",
+                new Contact("mmonahan", "https://github.com/mmonahan", "fake@email.com"),
+                "license",
+                "licenseUrl",
+                Collections.emptyList());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(apiInfo);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdvertiserApplication.class, args);
--- src/test/java/com/iheart/advertiser/AdvertiserApplicationTests.java
+++ src/test/java/com/iheart/advertiser/AdvertiserApplicationTests.java
@@ -1,16 +1,33 @@
 package com.iheart.advertiser;
 
+import com.iheart.advertiser.model.Advertiser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AdvertiserApplicationTests {
 
+    @Autowired
+    private AdvertiserRepository repository;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void getAllAdvertisers() {
+        List<Advertiser> advertiserList = repository.getAllAdvertisers();
+        assertNotNull(advertiserList);
+        assertFalse(advertiserList.isEmpty());
     }
 
 }
