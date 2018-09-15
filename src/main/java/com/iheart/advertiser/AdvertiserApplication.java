7b7043ed886d162d9669e52a809cc9478b60a7bd
mmonahan <mmonahan@email.com>
2018-09-15 15:05:28 -0500

    Converting tabs to spaces

--- src/main/java/com/iheart/advertiser/AdvertiserApplication.java
+++ src/main/java/com/iheart/advertiser/AdvertiserApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdvertiserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdvertiserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertiserApplication.class, args);
+    }
 }
