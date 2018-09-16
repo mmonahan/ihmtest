0e8500308ea274ff2d3f6b5f5af9613f2236b33a
mmonahan <mmonahan@email.com>
2018-09-15 21:28:53 -0500

    Jacoco added, improved test coverage

--- src/test/java/com/iheart/advertiser/AdvertiserApplicationTests.java
+++ src/test/java/com/iheart/advertiser/AdvertiserApplicationTests.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -13,21 +17,19 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AdvertiserApplicationTests {
 
     @Autowired
-    private AdvertiserRepository repository;
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void contextLoads() {
-    }
-
-    @Test
-    public void getAllAdvertisers() {
-        List<Advertiser> advertiserList = repository.getAllAdvertisers();
-        assertNotNull(advertiserList);
-        assertFalse(advertiserList.isEmpty());
+    public void testAllAdvertisers() {
+        ResponseEntity<List<Advertiser>> response =
+                restTemplate.exchange("/api/advertiser", HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<Advertiser>>(){});
+        assertNotNull(response.getBody());
+        assertFalse(response.getBody().isEmpty());
     }
 
 }
